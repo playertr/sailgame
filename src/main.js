a9ba27f73e4ad10ce73e709e2686dca88475b787
Tim Player <timrplayer@gmail.com>
2023-07-16 15:24:54 -0700

    Refactor for clarity.

--- src/main.js
+++ src/main.js
@@ -15,6 +15,128 @@ import wind_data from './data/json/gfs.json'
 
 const FRAME_RATE = 60;
 
+// *****************************************************************************
+// Define helper functions. ****************************************************
+
+// Resize the Wind and Game canvases when we zoom in on the olMap canvas
+// (takes desired width and height in pixels)
+function resizeCanvas(canvas, canvasWidth, canvasHeight) {
+  var ctx = canvas.getContext('2d', { willReadFrequently: true })
+  if (window.devicePixelRatio > 1) {
+
+    canvas.width = canvasWidth * window.devicePixelRatio;
+    canvas.height = canvasHeight * window.devicePixelRatio;
+    canvas.style.width = canvasWidth + "px";
+    canvas.style.height = canvasHeight + "px";
+
+    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+  }
+}
+
+// Transform from longitude,latitude to screen coordinates.
+var project_fn = function (lon, lat) {
+  return map.getPixelFromCoordinate(olProj.fromLonLat([lon, lat], 'EPSG:3857'))
+}
+
+// See whether a given pixel is on land
+var is_on_land = function (x, y) {
+  var on_land = true
+  map.forEachFeatureAtPixel([x, y], function (feature, layer) {
+    if (feature.getProperties()['featureclass'] == 'Ocean') {
+      on_land = false // returning true here didn't work
+    }
+  })
+  return on_land
+}
+
+// Restart the wind animation after changing the View and extents
+function refreshWindy() {
+  if (!windy) {
+    return;
+  }
+
+  windy.stop();
+
+  var mapSize = map.getSize();
+  var extent = map.getView().calculateExtent(mapSize);
+  extent = olProj.transformExtent(extent, 'EPSG:3857', 'EPSG:4326')
+
+  resizeCanvas(windCanvas, mapSize[0], mapSize[1]);
+  resizeCanvas(gameCanvas, mapSize[0], mapSize[1])
+
+  windy.start(
+    project_fn,
+    extent
+  );
+}
+
+// Handle keys
+function keyDownEvent(e) {
+  switch (e.keyCode) {
+    case 37: // left / a
+    case 65:
+      s.rudder = Math.max(s.rudder - 1, -1);
+      break;
+    case 38: // up / w
+    case 87:
+      s.drifting = false;
+      break;
+    case 39: // right / d
+    case 68:
+      s.rudder = Math.min(s.rudder + 1, 1);
+      break;
+    case 40: // down
+    case 83:
+      s.drifting = true;
+      break;
+    case 32:
+      if (gameActive == true) {
+        pauseGame();
+      }
+      else {
+        // pass
+      }
+      break;
+  }
+}
+
+// Update the wind animation and sailboat simulation.
+// Repeat this on a timed callback.
+function animate() {
+
+  windy.animate_frame(windCanvas)
+  s.animate_frame(gameCanvas)
+
+  try {
+    windy.timer = setTimeout(function () {
+      requestAnimationFrame(animate);
+    }, 1000 / FRAME_RATE);
+  }
+  catch (e) {
+    console.error(e);
+  }
+}
+
+// Wrapper around requestAnimationFrame --
+// shim layer with setTimeout fallback
+window.requestAnimationFrame = (function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 15);
+    };
+})();
+
+// *****************************************************************************
+// Declare variables and generally do stuff.************************************
+
+// Get canvas elements. ********************************************************
+var windCanvas = document.getElementById('windyMap')
+var gameCanvas = document.getElementById('gameMap')
+
 // Make a map with the oceans outlined in white. *******************************
 
 // define style spec
@@ -63,135 +185,25 @@ const map = new Map({
   }),
 });
 
-// function used to resize the Wind and Game canvases
-// when we zoom in on the olMap canvas
-// takes desired width and height in pixels
-function resizeCanvas(canvas, canvasWidth, canvasHeight) {
-  var ctx = canvas.getContext('2d', { willReadFrequently: true })
-  if (window.devicePixelRatio > 1) {
-
-    canvas.width = canvasWidth * window.devicePixelRatio;
-    canvas.height = canvasHeight * window.devicePixelRatio;
-    canvas.style.width = canvasWidth + "px";
-    canvas.style.height = canvasHeight + "px";
-
-    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
-  }
-}
-
-// function to transform from longitude,latitude to screen coordinates.
-var project_fn = function (lon, lat) {
-  return map.getPixelFromCoordinate(olProj.fromLonLat([lon, lat], 'EPSG:3857'))
-}
-
-// function to see whether a given pixel is on land
-var is_on_land = function (x, y) {
-  var on_land = true
-  map.forEachFeatureAtPixel([x, y], function (feature, layer) {
-    if (feature.getProperties()['featureclass'] == 'Ocean') {
-      on_land = false // returning true here didn't work
-    }
-  })
-  return on_land
-}
-
-var windCanvas = document.getElementById('windyMap')
-var windy;
-function refreshWindy() {
-  if (!windy) {
-    return;
-  }
-
-  windy.stop();
-
-  var mapSize = map.getSize();
-  var extent = map.getView().calculateExtent(mapSize);
-  extent = olProj.transformExtent(extent, 'EPSG:3857', 'EPSG:4326')
-
-  resizeCanvas(windCanvas, mapSize[0], mapSize[1]);
-  resizeCanvas(gameCanvas, mapSize[0], mapSize[1])
-
-  windy.start(
-    project_fn,
-    extent
-  );
-}
+// Define map behavior. ********************************************************
 
 map.on('moveend', refreshWindy);
-
-var gameCanvas = document.getElementById('gameMap')
-
 document.addEventListener("keydown", keyDownEvent)
 
-var s; // forward-declare
-
-function keyDownEvent(e) {
-  switch (e.keyCode) {
-    case 37: // left / a
-    case 65:
-      s.rudder = Math.max(s.rudder - 1, -1);
-      break;
-    case 38: // up / w
-    case 87:
-      s.drifting = false;
-      break;
-    case 39: // right / d
-    case 68:
-      s.rudder = Math.min(s.rudder + 1, 1);
-      break;
-    case 40: // down
-    case 83:
-      s.drifting = true;
-      break;
-    case 32:
-      if (gameActive == true) {
-        pauseGame();
-      }
-      else {
-        // pass
-      }
-      break;
-  }
-}
-
+// Make a WindDB: a function from (lon,lat) to (U,V) ***************************
 var wind_db = new WindDB(wind_data)
 
-windy = new Windy({ wind_db: wind_db });
+// Make a Windy: a particle-based wind animation *******************************
+var windy = new Windy({ wind_db: wind_db });
 
-s = new Sailboat(wind_db, project_fn, is_on_land);
+// Make a Sailboat: a stateful boat simulator **********************************
+var s = new Sailboat(wind_db, project_fn, is_on_land);
 s.lon = 0;
 s.lat = 0;
 s.hdg = Math.PI / 16;
 
-function animate() {
-
-  windy.animate_frame(windCanvas)
-  s.animate_frame(gameCanvas)
-
-  try {
-    windy.timer = setTimeout(function () {
-      requestAnimationFrame(animate);
-    }, 1000 / FRAME_RATE);
-  }
-  catch (e) {
-    console.error(e);
-  }
-}
-
+// Wait for the first map render, then start the game **************************
 map.once('postrender', function () {
   refreshWindy();
   animate();
 });
-
-
-// shim layer with setTimeout fallback
-window.requestAnimationFrame = (function () {
-  return window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function (callback) {
-      window.setTimeout(callback, 1000 / 15);
-    };
-})();
